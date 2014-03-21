330786225f8d02ccdfbd7178cf3bac52852d1bb1
tiiiot2014 <tii.iot2014@gmail.com>
2014-03-21 17:57:46 +0530

    Create searchTweets.php

---
+++ searchTweets.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'lib/twitteroauth.php';
+ 
+define('CONSUMER_KEY', 'your_consumer_key');
+define('CONSUMER_SECRET', 'your_consumer_secret');
+define('ACCESS_TOKEN', 'your_access_token');
+define('ACCESS_TOKEN_SECRET', 'your_access_token_secret');
+ 
+$toa = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+ 
+$query = array(
+  "q" => "#WorldSeries"
+);
+ 
+$results = $toa->get('search/tweets', $query);
+ 
+foreach ($results->statuses as $result) {
+  echo $result->user->screen_name . ": " . $result->text . "\n";
+}
+?>
